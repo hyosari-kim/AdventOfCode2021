c34ae6533517dc68eda726534d57cb7884c7de07
hyoeun Kim <hyoeunkim@M1-macbook-pro.local>
2021-12-01 22:30:01 +0900

    Add day1

---
+++ bin/day1/day1.ml
@@ -0,0 +1,32 @@
+open Lib
+
+let filename = "input/day1/puzzle.txt"
+let lines = Loader.read_file_to_string_list filename 
+let data = lines |> List.map(int_of_string)
+
+(*
+  Part 1: count the number of times a depth measurement increases
+*)
+
+let finalState = data
+  |> List.fold_left (fun acc d ->
+    match acc with
+    | (0, 0) -> (d, 0)
+    | (bef, cnt) -> (d, if bef < d then cnt + 1 else cnt)
+  ) (0, 0)
+
+let _ = let result = string_of_int (snd finalState) in print_string ("part1:" ^ result)
+
+(*
+  Part 2: consider sums of a three-measurement sliding window.
+*)
+
+let rec sum_of_til_nth n = function
+  | [] -> 0
+  | h :: t -> if n > 0 then h + sum_of_til_nth (n-1) t else 0
+
+let rec find_increses = function
+ | [] -> 0
+ | h :: t -> if (h + sum_of_til_nth 2 t) < (sum_of_til_nth 3 t) then 1 + find_increses t else find_increses t 
+
+let _ = let result = string_of_int (find_increses data) in print_string ("part2:" ^ result)
---
+++ lib/loader.ml
@@ -0,0 +1,14 @@
+open Core
+
+let rec read_line_rev ic lines =
+  let line = In_channel.input_line ic in
+    match line with
+    | None -> lines
+    | Some(str) -> read_line_rev ic (str::lines) 
+  
+
+let read_file filename =
+  let in_ch = In_channel.create filename in read_line_rev in_ch []
+
+let read_file_to_string_list filename =
+  read_file filename |> List.rev
